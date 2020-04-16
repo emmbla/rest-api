888943a5a504e599211fdfe9b88e4981740972b1
embla <emblajonsson@gmail.com>
2020-04-16 12:11:41 +0200

    fixed so you can post and put color, food mm

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const Joi = require('joi')
 const express = require('express')
 const app = express()
 app.use(express.json())
+const port = process.env.PORT || 3000
 
 // array ak "api"
 const cats = [
-  { id: 1, name: 'Sudden' },
-  { id: 2, name: 'Prins' },
-  { id: 3, name: 'Essi' },
-  { id: 4, name: 'Nuffe' },
-  { id: 5, name: 'Sindi' },
+  { id: 1, name: 'Sudden', color: "blue", food: "food"  },
+  { id: 2, name: 'Prins',color: "blue" , food: "food" },
+  { id: 3, name: 'Essi',color: "blue", food: "food"  },
+  { id: 4, name: 'Nuffe',color: "blue", food: "food"  },
+  { id: 5, name: 'Sindi',color: "blue", food: "food" },
 ]
 
 // what's shown on the screen when at the index page
@@ -23,11 +24,17 @@ app.get('/api/cats', (req, res) => {
   res.send(cats)
 })
 
+// get specific object
+app.get('/api/cats/:id', (req, res) => {
+  const cat = cats.find((c) => c.id === parseInt(req.params.id))
+  if (!cat)
+    return res.status(404).send('The cat with the given id was not found')
+  res.send(cat)
+})
+
 // add onother object
 app.post('/api/cats', (req, res) => {
-  const { error } = validateCourse(req.body)
-  if (error) return res.status(400).send(result.error.details[0].message)
-
+ 
   if (!req.body.name || req.body.name.length < 3) {
     res.status(400).send('Name is requierd and shuold be minimum 3 characters')
     return
@@ -36,7 +43,10 @@ app.post('/api/cats', (req, res) => {
   const cat = {
     id: cats.length + 1,
     name: req.body.name,
+    color: req.body.color,
+    food: req.body.food
   }
+
   cats.push(cat)
   res.send(cat)
 })
@@ -47,39 +57,24 @@ app.put('/api/cats/:id', (req, res) => {
   if (!cat)
     return res.status(404).send('The cat with the given id was not found')
 
-  const { error } = validateCourse(req.body)
-  if (error) return res.status(400).send(result.error.details[0].message)
+  cat.name = req.body.name,
+  cat.color = req.body.color,
+  cat.food = req.body.food
 
-  cat.name = req.body.name
   res.send(cat)
 })
 
-// get specific object
-app.get('/api/cats/:id', (req, res) => {
-  const cat = cats.find((c) => c.id === parseInt(req.params.id))
-  if (!cat)
-    return res.status(404).send('The cat with the given id was not found')
-  res.send(cat)
-})
-
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Running at http://localhost:${port}`))
-
-function validateCourse(cat) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  }
-  return Joi.validate(cat, schema)
-}
 
 // delete a object, if clicked twice the next is deleted. (1,2...)
 app.delete('/api/cats/:id', (req, res) => {
   const cat = cats.find((c) => c.id === parseInt(req.params.id))
   if (!cat)
-    return res.status(404).send('The coruse with the given id was not found')
-
+  return res.status(404).send('The coruse with the given id was not found')
+  
   const index = cats.indexOf(cat)
   cats.splice(index, 1)
-
+  
   res.send(cat)
 })
+
+app.listen(port, () => console.log(`Running at http://localhost:${port}`))
