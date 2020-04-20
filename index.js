35d2340c1bcd2860b5a9eae0b5da52b459b6d62d
embla <emblajonsson@gmail.com>
2020-04-20 14:04:54 +0200

    ,

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const port = 3000
 
 // array aka "api"
 const cats = [
-  { id: 1, name: 'Sudden', color: "Light Brown", food: "Royal Canin"  },
-  { id: 2, name: 'Prins',color: "Grey and White" , food: "Pure Naturals" },
-  { id: 3, name: 'Essi',color: "Dark Brown", food: "Orijen"  },
-  { id: 4, name: 'Nuffe',color: "Grey", food: "Purima"  },
-  { id: 5, name: 'Sindi',color: "Light Brown", food: "Mjau" },
+  { id: 1, name: 'Sudden', color: 'Light Brown', food: 'Royal Canin' },
+  { id: 2, name: 'Prins', color: 'Grey and White', food: 'Pure Naturals' },
+  { id: 3, name: 'Essi', color: 'Dark Brown', food: 'Orijen' },
+  { id: 4, name: 'Nuffe', color: 'Grey', food: 'Purima' },
+  { id: 5, name: 'Sindi', color: 'Light Brown', food: 'Mjau' },
 ]
 
-//index 
+//index
 app.get('/', (req, res) => {
   res.send('The cats')
 })
@@ -26,8 +26,7 @@ app.get('/api/cats', (req, res) => {
 // Specifikt objekt
 app.get('/api/cats/:id', (req, res) => {
   const cat = cats.find((c) => c.id === parseInt(req.params.id))
-  if (!cat)
-    return res.status(404).send('No cat with that ID.')
+  if (!cat) return res.status(404).send('No cat with that ID.')
   res.send(cat)
 })
 
@@ -36,9 +35,8 @@ app.post('/api/cats', (req, res) => {
   let newCat = req.body
   newCat.id = s4()
   cats.push(req.body)
-  res.status(201).send()
+  res.status(201).send(req.body)
 })
-
 
 // Uppdatera ett objekt
 app.put('/api/cats/:id', (req, res) => {
@@ -46,33 +44,31 @@ app.put('/api/cats/:id', (req, res) => {
   // Om id:t ej finns
   if (!cat)
     return res.status(404).send('The cat with the given id was not found')
-    
-// om id hittades
-  cat.name = req.body.name,
-  cat.color = req.body.color,
-  cat.food = req.body.food
+
+    // om id hittades
+  ;(cat.name = req.body.name),
+    (cat.color = req.body.color),
+    (cat.food = req.body.food)
   res.send(cat)
 })
-
 
 // Radera objekt
 app.delete('/api/cats/:id', (req, res) => {
   const cat = cats.find((c) => c.id === parseInt(req.params.id))
   if (!cat)
-  return res.status(404).send('The cat with the given id was not found')
-  
+    return res.status(404).send('The cat with the given id was not found')
+
   const index = cats.indexOf(cat)
   cats.splice(index, 1)
-  
+
   res.send(cat)
 })
 
-
 app.listen(port, () => console.log(`Running at http://localhost:${port}`))
 
-// random id 
+// random id
 let s4 = () => {
   return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+    .toString(16)
+    .substring(1)
 }
